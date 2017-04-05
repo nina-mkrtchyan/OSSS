5aa5a654836785a800ad4a83272202286c209786
ShShavarsh <shavarsh.sh@gmail.com>
2017-04-05 13:20:49 +0400

    1
    first

---
+++ Operatyng_System_CSharp_2017/HardWare/HardWare/CPU.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace HardWare
+{
+    
+    class CPU
+    {
+       public  static void Main(string[] args)
+        {
+           /* henc Cpu n miacav ..aysinqn sksav es 
+            * Main i katarum@,,petqa miangamic Timer in inicializacnenq
+           */
+
+
+        }
+    }
+}
